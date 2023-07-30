c12153e14b87d8fcb17dba9878f1080eb8d06c2f
miaochao666 <1446871885@qq.com>
2023-07-30 14:57:57 +0800

    feat:切片删除元素

--- test/01/main/main.go
+++ test/01/main/main.go
@@ -3,7 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	// 初始化十个元素，容量为十的切片
-	arr1 := make([]int, 10, 10)
+	// 初始化0个元素，容量为10的切片
+	arr1 := make([]any, 0, 10)
 	fmt.Println(arr1)
+	arr2 := []int{1, 2, 3, 4}
+	//arr2 = remove1(arr2, 1)
+	arr2 = remove2(arr2, 1)
+	fmt.Println(arr2)
+}
+
+func remove1[T AnyType](arr []T, index int) []T {
+	if index < 0 || len(arr) <= index {
+		return arr
+	}
+	newarr1 := arr[:index]
+	newarr2 := arr[index+1 : len(arr)]
+	newarr3 := append(newarr1, newarr2...)
+	return newarr3
+}
+
+func remove2[T AnyType](arr []T, index int) []T {
+	result := make([]T, 0)
+	for i, val := range arr {
+		if i != index {
+			result = append(result, val)
+		}
+	}
+	return result
+}
+
+type AnyType interface {
+	int | string
 }
